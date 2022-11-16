2e0225f1f9c2cff41054f50aeab4390b4df1bc14
Sivabajee <111592422+Sivabajee@users.noreply.github.com>
2022-11-16 16:23:40 -0600

    second commit

--- routes/mercedes_benz.js
+++ routes/mercedes_benz.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const mercedes_benz_controlers= require('../controllers/mercedes_benz');
 var router = express.Router();
-/* GET costumes */
+/* GET mercedes_benzs */
 router.get('/', mercedes_benz_controlers.mercedes_benz_view_all_Page );
-// GET request for one costume. 
+// GET request for one mercedes_benz. 
 router.get('/mercedes_benz/:id', mercedes_benz_controlers.mercedes_benz_detail); 
+/* GET detail mercedes_benz page */ 
+router.get('/detail', mercedes_benz_controlers.mercedes_benz_view_one_Page); 
 module.exports = router;
