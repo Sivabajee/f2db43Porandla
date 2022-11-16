e237d5521774da97f0a883e1182c1cd269b96300
Sivabajee <111592422+Sivabajee@users.noreply.github.com>
2022-11-16 16:07:47 -0600

    delete

--- routes/resource.js
+++ routes/resource.js
@@ -18,3 +18,17 @@ router.get('/mercedes_benz/:id', merecedes_benz_controller.mercedes_benz_detail)
 // GET request for list of all mercedes_benz items.
 router.get('/mercedes_benz', merecedes_benz_controller.mercedes_benz_list);
 module.exports = router;
+
+
+// Handle Costume delete on DELETE. 
+exports.costume_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Costume.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+};
