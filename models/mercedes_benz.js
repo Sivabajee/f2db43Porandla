57caa87c06af9a38229bc88b6732c19c4ead1f3e
Sivabajee <111592422+Sivabajee@users.noreply.github.com>
2022-11-30 23:24:27 -0600

    final  commit

--- models/mercedes_benz.js
+++ models/mercedes_benz.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose")
 const mercedes_benzSchema = mongoose.Schema({
-    mercedes_benz_luxurious_name: String,
-    mercedes_benz_tire_size: Number,
-    mercedes_benz_High_Beam_type: String
+    mercedes_benz_luxurious_name: {
+        type:String,
+        required :true
+    },
+    mercedes_benz_tire_size: {
+        type:Number,
+        min:5,max:500
+    },
+    mercedes_benz_High_Beam_type: {
+        type:String,
+        required:true
+    }
 })
 module.exports = mongoose.model("mercedes_benz",mercedes_benzSchema)
--- routes/mercedes_benz.js
+++ routes/mercedes_benz.js
@@ -8,16 +8,26 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const mercedes_benz_controlers= require('../controllers/mercedes_benz');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET mercedes_benzs */
 router.get('/', mercedes_benz_controlers.mercedes_benz_view_all_Page );
 // GET request for one mercedes_benz. 
 router.get('/mercedes_benz/:id', mercedes_benz_controlers.mercedes_benz_detail); 
 /* GET detail mercedes_benz page */ 
-router.get('/detail', mercedes_benz_controlers.mercedes_benz_view_one_Page); 
+router.get('/detail',secured, mercedes_benz_controlers.mercedes_benz_view_one_Page); 
 /* GET create mercedes_benz page */ 
-router.get('/create', mercedes_benz_controlers.mercedes_benz_create_Page); 
+router.get('/create',secured, mercedes_benz_controlers.mercedes_benz_create_Page); 
 /* GET create update page */ 
-router.get('/update', mercedes_benz_controlers.mercedes_benz_update_Page); 
+router.get('/update',secured , mercedes_benz_controlers.mercedes_benz_update_Page); 
 /* GET delete mercedes_benz page */ 
-router.get('/delete', mercedes_benz_controlers.mercedes_benz_delete_Page);
+router.get('/delete',secured, mercedes_benz_controlers.mercedes_benz_delete_Page);
 module.exports = router;
